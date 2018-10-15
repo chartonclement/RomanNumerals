6ba15057fb510dcf055cdad653cd6842b38830b9
chartonclement <43209309+chartonclement@users.noreply.github.com>
2018-10-15 11:50:57 +0300

    assigment finish

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,87 @@
 
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
+	
+	
+	public boolean NotaromainNumerals(String oneromanNum) {
+		if(oneromanNum != "I" && oneromanNum != "V" && oneromanNum != "X" && oneromanNum != "C" && oneromanNum != "D" && oneromanNum != "M") {
+	    	  return true;
+	      }
+		else {return false;}
+		
+	}
+	public char letter(String num, int i) {
+		char romanNum = num.charAt(i);
+		return romanNum;
+	}
+	public int transf(String num, int i) {
+		int valeur=0;
+		char romanNum = letter(num,i);
+		if (romanNum=='I') {
+			valeur = 1;
+		}
+		else if (romanNum=='V') {
+			valeur = 5; 
+		}
+		else if (romanNum=='X') {
+			valeur = 10; 
+		}
+		else if (romanNum=='L') {
+			valeur = 50; 
+		}
+		else if (romanNum=='C') {
+			valeur = 100; 
+		}
+		else if (romanNum=='D') {
+			valeur = 500; 
+		}
+		else if (romanNum=='M') {
+			valeur = 1000;
+		}
+		return valeur;	
+	}
+	public int addition(String num,int i) {
+		char romanNum = letter(num,i);
+		if((num.length()-i)>=4) {
+			if (romanNum=='I' || romanNum=='X' ||romanNum=='C'|| romanNum=='M') {
+				if ((letter(num,i+3))==romanNum) {
+					return 0;
+				}
+			}
+		}
+		if((num.length()-i)>=2) {
+			if (romanNum=='V' || romanNum=='L' ||romanNum=='D') {
+				if ((letter(num,i+1))==romanNum) {
+					return 0;
+				}
+			}
+		}
+		return 1;
+	}
+	public int converti(String romanNum) {
+		int sum=0;
+		for (int i=0; i< romanNum.length(); i++) {
+			if (addition(romanNum,i)==0) {
+				return 0;
+			}
+			int val = transf(romanNum, i);
+			if(i<romanNum.length()-1) {
+				int j=i+1;
+			int val2 =transf( romanNum,j);
+			if (val<val2) {
+			int val3=val2-val;
+			sum=sum+val3;
+			i++;
+			}
+			else{
+			sum=sum+val;	
+			}
+			}
+			else {
+				sum=sum+val;
+			}
+		}
+		return sum;
 		// To be Implemented
-		return 0;
 		
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,90 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_I() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(1, sc.converti("I"));
+	}
+	@Test
+	public void test_III() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(3, sc.converti("III"));
+	}
+	@Test
+	public void test_letter() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals('I', sc.letter("III",2));
+	}
+	@Test
+	public void test_letter2() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(1, sc.transf("III",2));
+	}
+	@Test
+	public void test_V() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(10, sc.converti("X"));
+	}
+	@Test
+	public void test_IV() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(4, sc.converti("IV"));
+	}
+	@Test
+	public void test_IX() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(9, sc.converti("IX"));
+	}
+	@Test
+	public void test_XL() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(40, sc.converti("XL"));
+	}
+	@Test
+	public void test_CM() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(900, sc.converti("CM"));
+	}
+	@Test
+	public void test_CD() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(400, sc.converti("CD"));
+	}
+	@Test
+	public void test_chiffre() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(true, sc.NotaromainNumerals("8"));
+	}
+	@Test
+	public void test_only_romain_letter() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(true, sc.NotaromainNumerals("e"));
+	}
+	
+	@Test
+	public void test_more_than_3() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(0, sc.converti("CCCC"));  // if we have more than 3 I,C,X,M this is not a roman number and the program return 0
+	}
+	@Test
+	public void test_more_than_one() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(0, sc.converti("VV"));   // if we have more than 1 V,D this is not a roman number and the program return 0
+	}
+	@Test
+	public void test_X() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(5, sc.converti("V"));
+	}
+	@Test
+	public void test_1984() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(1984, sc.converti("MCMLXXXIV"));
+	}
+	@Test
+	public void test_2014() {
+		RomanNumerals sc = new RomanNumerals();
+		assertEquals(2014, sc.converti("MMXIV"));
 	}
 
 }
